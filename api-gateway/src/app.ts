1a4ce06107b2c5d533ba9ca32ec776f64a59d8ae
ekose <ertugrulkose1235@gmail.com>
2025-08-03 10:28:11 +0000

    api gateway created

---
+++ api-gateway/src/app.ts
@@ -0,0 +1,29 @@
+import Fastify from "fastify";
+import * as dotenv from 'dotenv';
+import proxy from '@fastify/http-proxy'
+dotenv.config();
+
+const app = Fastify({ logger: true });
+const port: number = +(process.env.PORT || "3000");
+
+
+app.register(proxy, {
+	upstream : process.env.AUTH_SERVICE_URL || 'http://localhost:3001',
+	prefix: "auth/",
+	rewritePrefix : ''
+});
+
+const start = async () => {
+  try {
+    await app.listen({ port, host: "0.0.0.0" });
+    console.log(`Api-gateway ${port} portunda çalıştı`);
+  } catch (error) {
+    console.log({
+      message: "Api gateway sunucusu çalıştırılırken sorun oluştu:",
+      error,
+    });
+    process.exit(1);
+  }
+};
+
+start();
